3491241642f4ea8da9e1a8dcc5f9b99d56593d29
luuk0406 <luukf0406@gmail.com>
2023-02-05 00:32:38 +0100

    changes to controller + basic error handling

--- RestApi/Controllers/SupportTickerController.cs
+++ RestApi/Controllers/SupportTickerController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RestApi.Contracts.SupportTicket;
 using RestApi.Models;
+using RestApi.Services.Tickets;
 
 namespace RestApi.Controllers;
 
@@ -8,6 +9,13 @@ namespace RestApi.Controllers;
 [Route("tickets")]
 public class SupportTicketController : ControllerBase 
 {
+    private readonly ITicketService _ticketService;
+
+    public SupportTicketController(ITicketService ticketService)
+    {
+        _ticketService = ticketService;
+    }
+
     [HttpPost]
     public IActionResult CreateTicket(CreateTicketRequest request) 
     {
@@ -20,6 +28,8 @@ public class SupportTicketController : ControllerBase
             request.Tags
         );
 
+        _ticketService.CreateTicket(ticket);
+
         var response = new TicketResponse(
             ticket.Id,
             ticket.Username,
@@ -28,28 +38,51 @@ public class SupportTicketController : ControllerBase
             ticket.SubmitDate,
             ticket.Tags
         );
-        
+
         return CreatedAtAction(
-            nameof(ReturnTicket),
-            new { id = ticket.Id },
-            response);
+            actionName: nameof(ReturnTicket),
+            routeValues: new { id = ticket.Id },
+            value: response);
     }
 
     [HttpGet("{id:guid}")]
     public IActionResult ReturnTicket(Guid id) 
     {
-        return Ok(id);
+        SupportTicket ticket = _ticketService.GetTicket(id);
+
+        var response = new TicketResponse(
+            ticket.Id,
+            ticket.Username,
+            ticket.Email,
+            ticket.Description,
+            ticket.SubmitDate,
+            ticket.Tags
+        );
+
+        return Ok(response);
     }
 
     [HttpPut("{id:guid}")]
     public IActionResult UpsertTicket(Guid id, UpsertTicketRequest request) 
     {
-        return Ok(request);
+            var ticket = new SupportTicket(
+            id,
+            request.Username,
+            request.Email,
+            request.Description,
+            request.SubmitDate,
+            request.Tags
+        );
+        _ticketService.UpsertTicket(ticket);
+
+        //return 201 if req == new
+        return NoContent();
     }
 
     [HttpDelete("{id:guid}")]
     public IActionResult DeleteTicket(Guid id) 
     {
-        return Ok(id);
+        _ticketService.DeleteTicket(id);
+        return NoContent();
     }
 }
--- RestApi/Program.cs
+++ RestApi/Program.cs
@@ -1,10 +1,14 @@
+using RestApi.Services.Tickets;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services.AddScoped<ITicketService, TicketService>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
